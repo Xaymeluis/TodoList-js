5439d8dbca7bb26b92987a8886fbe4a49115e238
Xayme <lalarcon@clinicarequipa.com.pe>
2021-09-18 14:42:57 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { Todo, TodoList } from "./classes";
+import { crearTodoHtml } from "./js/componentes";
+
+import "./styles.css";
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearTodoHtml);
+
+todoList.todos[0].imprimirClase();
+
+console.log("todos", todoList.todos);
